1e56077c7d5dadccce64caf7ca7e1c6321a50fe0
Olga Efimova <ov.shaikina@gmail.com>
2026-03-03 21:09:30 +0200

    Homework 13

--- src/test/java/ui_tests/NavigationTests.java
+++ src/test/java/ui_tests/NavigationTests.java
@@ -11,5 +11,21 @@ public class NavigationTests extends AppManager {
     public void iconFacebookNavigationTest(){
         Assert.assertTrue(new HomePage(getDriver())
                 .clickIconFooter(utils.enums.FooterMenuItem.ICON_FACEBOOK, "Facebook"));
+    }@Test
+    public void iconVKNavigationTest(){
+        Assert.assertTrue(new HomePage(getDriver())
+                .clickIconFooter(FooterMenuItem.ICON_VK, "ВКонтакте"));
+    }@Test
+    public void iconInstagramNavigationTest(){
+        Assert.assertTrue(new HomePage(getDriver())
+                .clickIconFooter(FooterMenuItem.ICON_INSTAGRAM, "Instagram"));
+    }@Test
+    public void iconSlackNavigationTest(){
+        Assert.assertTrue(new HomePage(getDriver())
+                .clickIconFooter(FooterMenuItem.ICON_SLACK, "Slack"));
+    }@Test
+    public void iconTelegramNavigationTest(){
+        Assert.assertTrue(new HomePage(getDriver())
+                .clickIconFooter(FooterMenuItem.ICON_TELEGRAM, "Telegram messenger"));
     }
 }
