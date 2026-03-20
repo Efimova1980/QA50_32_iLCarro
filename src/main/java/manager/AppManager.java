0a301b93df904ebc2c144f0e04a61ffb2b44d3fb
Olga Efimova <ov.shaikina@gmail.com>
2026-03-20 12:38:33 +0200

    workflow

--- src/main/java/manager/AppManager.java
+++ src/main/java/manager/AppManager.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.support.events.WebDriverListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import utils.WDListener;
 
@@ -21,36 +20,25 @@ public class AppManager {
     public final static Logger logger = LoggerFactory.getLogger(AppManager.class);
 
     public WebDriver getDriver() {
-        if (driver == null) {
-            logger.info("Driver is not initialized, create driver...");
-            initDriver();
-        }
         return driver;
     }
 
-    @BeforeClass(alwaysRun = true)
+    @BeforeMethod(alwaysRun = true)
     public void setup(){
         logger.info("Start testing: " + LocalDate.now() + " : " + LocalTime.now());
-        initDriver();
+        WebDriverListener webDriverListener = new WDListener();
 
-    }
-
-    private void initDriver() {
-        if (driver == null){
-            logger.info("Create driver...");
-            WebDriverListener webDriverListener = new WDListener();
-
-//            ChromeOptions options = new ChromeOptions();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--lang=en");
 //            options.addArguments("--headless=new");
 //            options.addArguments("--no-sandbox");
 //            options.addArguments("--disable-dev-shm-usage");
 
-            driver = new ChromeDriver();
-            driver = new EventFiringDecorator<>(webDriverListener).decorate(driver);
-            driver.manage().window().maximize();
-            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-
-        }
+        driver = new ChromeDriver(options);
+        driver = new EventFiringDecorator<>(webDriverListener)
+                .decorate(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
     }
 
     @AfterMethod(enabled = true,alwaysRun = true)
